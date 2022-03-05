f6299eb1fd01716c39723cad7ef920ed416d1a39
IFrzzl <83833754+IFrzzl@users.noreply.github.com>
2022-03-05 20:00:57 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//Langing Bar skip content animation
+
+$("#down-arrow").click(function () {
+	$("#down-arrow").addClass("arrow-rotate-animation");
+});
+$("#down-arrow").on(
+	"animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
+	function () {
+		$(this).removeClass("arrow-rotate-animation");
+	}
+);
+
+//--------------------------
+//Dropdown code
+
+$("#drop-btn").click(function () {
+	$("#dropdown").show();
+	document.addEventListener(
+		"mouseup",
+		function (event) {
+			if (!event.target.classList.contains("dropdown-content")) {
+				$("#dropdown").hide();
+			}
+		},
+		true
+	);
+	document.removeEventListener("mouseup", function (event) {}, true);
+});
+
+function darkmode() {
+	if ($("#circle").hasClass("light")) {
+		$("*:not(.keeplight)").removeClass("darkmode");
+		$("#cover").addClass("sundown");
+		$("#cover").removeClass("sunup");
+		$("#circle").removeClass("light");
+	} else {
+		$("*:not(.keeplight)").addClass("darkmode");
+		$("#cover").toggleClass("sunup sundown");
+		$("#circle").addClass("light");
+	}
+}
+
+$("#cover").addClass("sundown");
+$("#darkmode-btn").click(function () {
+	darkmode();
+});
